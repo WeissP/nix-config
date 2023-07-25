f1b3e6eb72c46b088dad8c192702d13e9097b036
weiss <kingbaiing@163.com>
2023-07-25 20:25:28 +0200

    add emacs config

---
+++ home-manager/common/emacs/configs/weiss_dante_settings.el
@@ -0,0 +1,5 @@
+(with-eval-after-load 'dante
+  (setq dante-methods '(bare-cabal))
+)
+
+(provide 'weiss_dante_settings)
---
+++ home-manager/common/emacs/configs/weiss_tla-tools_settings.el
@@ -0,0 +1,5 @@
+(with-eval-after-load 'tla-tools
+  (require 'tla-pcal-mode)
+)
+
+(provide 'weiss_tla-tools_settings)
