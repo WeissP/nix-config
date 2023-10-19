2e371e4630bd9ad479b77b7b50c1465acacaf457
weiss <kingbaiing@163.com>
2023-10-19 17:12:36 +0200

    try to avoid pdf restore bug

--- home-manager/common/emacs/configs/weiss_pdf-view-restore_settings.el
+++ home-manager/common/emacs/configs/weiss_pdf-view-restore_settings.el
@@ -2,5 +2,18 @@
 
 (add-hook 'pdf-view-mode-hook 'pdf-view-restore-mode)
 
+(with-eval-after-load 'pdf-view-restore
+  (defun weiss-pdf-view-restore ()
+    "Restore page only if page is valid."
+    (when (derived-mode-p 'pdf-view-mode)
+      ;; This buffer is in pdf-view-mode
+      (let ((page (pdf-view-restore-get-page)))
+        (when (and page (<= page (pdf-cache-number-of-pages)))
+          (pdf-view-goto-page page)))))
+
+  (advice-add 'pdf-view-restore :override #'weiss-pdf-view-restore)
+  )
+
+
 ;; parent: 
 (provide 'weiss_pdf-view-restore_settings)
