39af809d0399f7cf23029a9c9445deddd0f1a441
weiss <kingbaiing@163.com>
2023-10-30 21:00:39 +0100

    add options regarding clean generations

--- nixos/common/minimum.nix
+++ nixos/common/minimum.nix
@@ -37,6 +37,11 @@
               keep-outputs = true
               keep-derivations = true
             '';
+            gc = {
+              automatic = true;
+              dates = "weekly";
+              options = "--delete-older-than 7d";
+            };
           }
           (ifDarwin {
             extraOptions = ''
