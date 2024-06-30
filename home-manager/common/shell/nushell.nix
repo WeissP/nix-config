08fe628661c50a91654001fbff20c3c6fc3957d6
weiss <kingbaiing@163.com>
2024-06-30 14:09:01 +0200

    make nushell fully isolate as atuin can be used

--- home-manager/common/shell/nushell.nix
+++ home-manager/common/shell/nushell.nix
@@ -3,9 +3,16 @@ with myEnv; {
   home.packages = [ pkgs.nufmt ];
   programs.nushell = lib.mkMerge [{
     enable = true;
+    # check options in https://github.com/nushell/nushell/blob/main/crates/nu-utils/src/sample_config/default_config.nu
     configFile.text = ''
       $env.config = {
          show_banner: false
+         render_right_prompt_on_last_line: false
+         history: {
+             sync_on_enter: false 
+             file_format: "sqlite"
+             isolation: true
+         }
       }
       $env.config = ($env.config | upsert hooks.env_change.PWD {|config|
               [{
